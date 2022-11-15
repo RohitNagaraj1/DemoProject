b340d8fbfd2b78a271b5d73f61721179227da305
RohitNagaraj1 <rohitnagaraj98@gmail.com>
2022-11-15 18:58:11 +0530

    LCS

--- DP/LCS.java
+++ DP/LCS.java
@@ -31,6 +31,10 @@ public class LCS
         
     }
 
+    // 0 0 0 0 
+    // 0 1 0 0
+    // 0 0 0 0 
+
     public static int LCSTabulation(char[] s1, char[] s2, int m, int n)
     {
         int[][] dp = new int[s1.length+1][s2.length+1];
@@ -48,6 +52,15 @@ public class LCS
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
+        }
+
+        for (int i = 0; i <= s1.length; i++)
+        {
+            for (int j = 0; j <= s2.length; j++)
+            {
+                System.out.print(dp[i][j] + "   ");
+            }
+            System.out.println();
         }
 
         return dp[s1.length][s2.length];
---
+++ DP/MostFrequentChar.java
@@ -0,0 +1,55 @@
+public class MostFrequentChar {
+    
+    public static char FindMostFrequentVowel(String str)
+    {
+        int[] arr = new int[26];
+
+        for (char ch : str.toCharArray())
+        {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                int index = ch - 'a';
+                arr[index]++;
+            }
+        }
+
+        char res = 'a';
+        int count = 0;
+        for (int i = 0; i < 26; i++)
+        {
+            if (count < arr[i])
+            {
+                count = arr[i];
+                res = (char)('a' + i);
+            }
+        }
+
+        return res;
+    }
+
+    public static int LengthContiguousBinary(String binary)
+    {
+        int max = 0;
+        int current = 0;
+
+        for (char ch : binary.toCharArray())
+        {
+            if (ch == '1')
+            {
+                current++;
+                max = Math.max(max, current);
+            }
+            else
+            {
+                current = 0;
+            }
+        }
+
+        return max;
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println(FindMostFrequentVowel("eeeeeee"));
+        System.out.println(LengthContiguousBinary("10111111111101111111111"));
+    }
+}
